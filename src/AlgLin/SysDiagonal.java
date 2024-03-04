e0161429f811886e4d056e6c3e62c385cc2016b8
sa204389 <akramslimani277@gmail.com>
2024-03-04 14:43:31 +0100

    finito

--- src/AlgLin/SysDiagonal.java
+++ src/AlgLin/SysDiagonal.java
@@ -51,7 +51,7 @@ public class SysDiagonal extends SysLin {
 			e.printStackTrace();
 		}
 		
-		System.out.println("****************    test de la norm ||Ax -b||    **************\n");
+		System.out.println("****************    test de la norme ||Ax -b||    **************\n");
 
 		Vecteur v1 = new Vecteur(tab2.length);
 		Vecteur v2 = new Vecteur(tab2.length);
