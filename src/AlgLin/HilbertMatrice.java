1588301c5c6d0d6b743ce4808c05de21bdf53b11
sa204389 <akramslimani277@gmail.com>
2024-03-09 15:12:13 +0100

    test de la classe

--- src/AlgLin/HilbertMatrice.java
+++ src/AlgLin/HilbertMatrice.java
@@ -1,45 +1,41 @@
 package AlgLin;
 
 public class HilbertMatrice extends Matrice {
-    public HilbertMatrice(int ordre) {
-        super(ordre, ordre);
-        for (int i = 0; i < nbLigne(); i++) {
-            for (int j = 0; j < nbLigne(); j++) {
-                remplacecoef(i, j, (double) 1 / (double) (i + j + 1));
 
-            }
-        }
-    }
+	public HilbertMatrice(int ordre) {
+		super(ordre, ordre);
+		for (int i = 0; i < nbLigne(); i++) {
+			for (int j = 0; j < nbLigne(); j++) {
+				remplacecoef(i, j, (double) 1 / (double) (i + j + 1));
+			}
+		}
+	}
 
-    public static void main(String[] args) throws Exception {
-        int ordre = 3;
-        HilbertMatrice hilbert = new HilbertMatrice(ordre);
-        System.out.println(hilbert);
-        Matrice inverse = hilbert.inverse();
-        System.out.println(inverse);
+	public static void main(String[] args) throws Exception {
+		int ordre = 3;
+		HilbertMatrice hilbert = new HilbertMatrice(ordre);
+		System.out.println(hilbert);
+		Matrice inverse = hilbert.inverse();
+		System.out.println(inverse);
 
-        Matrice produit = Matrice.produit(hilbert, inverse);
-        System.out.println(produit);
+		Matrice produit = Matrice.produit(hilbert, inverse);
+		System.out.println(produit);
 
-        System.out.println("Cond(Id) = ||id|| = " + hilbert.identite().cond_1());
+		System.out.println("Cond(Id) = ||id|| = " + hilbert.identite().cond_1());
 
-        System.out.println("Cond1(A) = ||A * A^-1||1 " + produit.norme_1());
+		System.out.println("conditionnement par la norme L1 : \n Cond1(A) = ||A * A^-1||1 " + produit.norme_1() + "\n");
 
-        System.out.println("CondINF(A) = ||A * A^-1||inf  = " + produit.norme_inf());
-        System.out.println(
-                "Résultat :  Le conditionnement de cette matrice est relativement trés faible et indique qu'elle est trés bien conditionnée. Les solutions aux systèmes linéaires Ax = b associés à cette matrice sont stables et précises, quelles que soient les valeurs de b.");
+		System.out.println("conditionnement par la norme L∞ : \n CondINF(A) = ||A * A^-1||inf  = " + produit.norme_inf() + "\n");
 
-        for (int i = 3; i <= 15; i++) {
-            
-            hilbert = new HilbertMatrice(i);
-            inverse = hilbert.inverse();
-            produit = Matrice.produit(hilbert, inverse);
-            System.out.println("Ordre = "+i);
-            System.out.println("Cond1(A) = ||A * A^-1||1 " + produit.norme_1());
-            System.out.println("CondINF(A) = ||A * A^-1||inf  = " + produit.norme_inf());
-            System.out.println("******");
-        }
+		for (int i = 3; i <= 15; i++) {
+			hilbert = new HilbertMatrice(i);
+			inverse = hilbert.inverse();
+			produit = Matrice.produit(hilbert, inverse);
+			System.out.println("************     Ordre = " + i + "*************");
+			System.out.println("Cond1(A) = ||A * A^-1||1 " + produit.norme_1());
+			System.out.println("CondINF(A) = ||A * A^-1||inf  = " + produit.norme_inf() + "\n");
 
-    }
+		}
+	}
 
 }
